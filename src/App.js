36016b30301d7175022ff335fe69d4f1c2d016bc
karthikgpi1 <karthikgopi024@gmail.com>
2022-08-30 18:04:38 +0530

    use state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+  // use state types
+
+
+import { React, useState } from 'react';
 
 function App() {
+  const [name, setName] = useState("karthik");
+  const [Flag, setFlag] = useState("false");
+  const [steps, setSteps] = useState(0);
+  const [names, setNames] = useState([]);
+
+  function changeName (){
+    console.log("clicked");       //name change type1
+    return setName("white");
+  
+  }
+
+  function changeName (){
+    console.log("clicked");        //name change type2
+    return setFlag(!Flag);
+  }
+   //----------------------------------
+  function increment (){
+    console.log("clicked");            
+    return setSteps(steps + 1);
+  }
+
+  function decrement (){
+    console.log("clicked");
+    return setSteps(steps - 1);
+  }
+
+  //----------------------
+
+  function addnames(e){
+    e.preventDefault();
+     setNames([...names,{id: names.length, name}]);
+     setName("");
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+         <div> hello,{name}</div>
+         <button onClick={changeName}>Click me</button>
+         <hr></hr>
+         //-------
+         <div> hello,{Flag ? "" : name}</div>
+         <button onClick={changeName}>Click me</button>
+         <hr></hr>
+
+         //-----------------------------
+         <button onClick={increment}>+</button>
+         <div>{steps}</div>
+         <button onClick={decrement}>-</button>
+         //----------------------------- 
+         <hr></hr>
+         <form onSubmit={addnames}>
+          <input type="text" value={name} placeholder="add names"
+           onChange={(e) => setName(e.target.value)}/>
+          <button>submit</button>
+         </form>
+         <hr></hr>
+         <ul>
+          {names.map((item) => (
+            <li key={item.id}>{item.name}</li>
+          ))}
+         </ul>
     </div>
   );
 }
